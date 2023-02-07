0a35e9c3283093d357b5369ef66320a42b335dbb
windvalley <i@sre.im>
2023-02-07 17:11:23 +0800

    docs: add CHANGELOG.md

--- internal/cmd/apiserver/apiserver.go
+++ internal/cmd/apiserver/apiserver.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-	"github.com/gogf/gf/v2/os/genv"
 	"github.com/gogf/gf/v2/os/glog"
 
 	"gf2-demo/utility"
@@ -59,10 +58,6 @@ var (
 
 			// 异步打印日志 & 显示打印错误的文件行号, 对访问日志无效
 			g.Log().SetFlags(glog.F_ASYNC | glog.F_TIME_STD | glog.F_FILE_LONG)
-
-			// 简化报错堆栈, 使不包含gf框架的部分, 仅对访问日志有效
-			envGfGerrorBrief := genv.Get("GF_GERROR_BRIEF")
-			g.Log().Debugf(ctx, "GF_GERROR_BRIEF=%s", envGfGerrorBrief)
 
 			configFile := g.Cfg().GetAdapter()
 			g.Log().Debugf(ctx, "use config file: %+v", configFile)
