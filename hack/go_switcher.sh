1683247b76fc1edd155669c137fa6b4e9c3ac7ed
windvalley <levinwang6@gmail.com>
2023-10-23 18:46:39 +0800

    chore: optimize hack/go_switcher.sh

--- hack/go_switcher.sh
+++ hack/go_switcher.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 # go_switcher.sh
 #
-# Switch between Go versions.
+# Go Multi-version Switching Management Tool.
 #
-# NOTE: Only tested on macOS.
+# NOTE: 1. Only tested on macOS; 2. Use Oh My Zsh.
 #
-# How to install Go: brew install go@1.20
+# How to install Go:
+#   brew install go@1.17
+#   brew install go@1.21
+#
+# How to install this script:
+#   cp ./go_switcher.sh /usr/local/bin/gov
+#
+# Usage example:
+#   $ gov -l
+#   $ gov -s 1.21
+
+GO_BIN_PATH="/usr/local/opt/go/bin"
 
 show_go_versions() {
     echo "Current Go version: 
@@ -14,33 +25,28 @@ $(go version)
 Go versions installed on this system:"
 
     # shellcheck disable=SC2125
-    go_list=/usr/local/opt/go@*
+    go_list=$(ls -d /usr/local/opt/go@*)
     for version in $go_list; do
-        echo "* $(basename "$version")"
+        version=$(echo "$version" | grep -Eo "[0-9]+\.[0-9]+")
+        echo "* $version"
     done
 }
 
 switch_go_version() {
     local version="$1"
-    if echo "$version" | grep -q 'go@'; then
-        version=$(echo "$version" | cut -d'@' -f2)
-    fi
-
-    if [[ -z "$version" ]]; then
-        echo "Error: no Go version specified"
+    if ! echo "$version" | grep -qE '^[0-9]+\.[0-9]+$'; then
+        echo -e "Error: invalid Go version specified -- $version\n"
+        show_go_versions
         return 1
     fi
 
     if [[ ! -d "/usr/local/opt/go@$version" ]]; then
-        echo "Error: Go version $version not found"
+        echo -e "Error: Go version not found -- $version\n"
+        show_go_versions
         return 1
     fi
 
-    echo "$PATH" | grep -q /usr/local/opt/go/bin || {
-        export PATH=/usr/local/opt/go/bin:$PATH
-        echo "export PATH=/usr/local/opt/go/bin:\$PATH" >>~/.zshrc
-    }
-
+    # Switch to the specified Go version.
     unlink /usr/local/opt/go
     ln -sf "/usr/local/opt/go@$version" /usr/local/opt/go
 
@@ -48,7 +54,9 @@ switch_go_version() {
 }
 
 show_help() {
-    echo "Usage:
+    echo "Go Multi-version Switching Management Tool.
+
+Usage:
 
     -l            Show all installed Go versions
     -s <version>  Switch to the specified Go version
@@ -61,6 +69,17 @@ Example:
     $ $0 -s 1.20"
 }
 
+# Make sure $GO_BIN_PATH is in $PATH.
+if ! echo "$PATH" | grep -q "$GO_BIN_PATH"; then
+    if ! grep -q "PATH=\"$GO_BIN_PATH:" ~/.zshrc; then
+        echo "export PATH=\"$GO_BIN_PATH:\$PATH\"" >>~/.zshrc
+    fi
+
+    echo "Warning: please execute 'omz reload' or 'exec zsh' first"
+    exit 1
+fi
+
+# Parse command line options.
 while getopts "ls:h" option; do
     case $option in
     l)
@@ -80,6 +99,7 @@ while getopts "ls:h" option; do
     esac
 done
 
+# Show help if no options were specified.
 show_help
 
 exit 0
